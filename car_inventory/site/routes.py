4f07026e88868a8a237dddf1f6997a32ba3ff472
Tenzin Lama <tenchoelama80@gmail.com>
2023-01-31 15:36:40 -0600

    week6

---
+++ car_inventory/site/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__,template_folder ='site_templates')
+
+"""
+Note that in the above code, some arguements are specified when creating the 
+Blueprint object. The first arguement, 'site', is the Blueprint's name, this is used 
+by Flask's routing mechanism. The second arguement, __name__, is the Blueprint's import name,
+which Flask uses to locate the Blueprint's resources 
+"""
+
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/profile')
+def profile():
+    return render_template('profile.html')
---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Give access to the project in ANY Operating System we find ourselves in 
+#Allows outside files/folders to be added to the project
+# from the base directory 
+
+class Config():
+    """
+    Set config varibales for the flask app.
+    Using Environment Variables where available otherwise
+    create the config variable if not done already
+    
+    """
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Wrong guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #turn.off.update.messages.from.sqlaclhemy
